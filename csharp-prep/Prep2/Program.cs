be8a0aca257fbc1f32931949dc129086a85ed1f6
anderJoe365 <anderson.joseph3656@gmail.com>
2024-01-09 07:11:04 -0700

    percentage to grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        String letters = "";
+
+        Console.Write("What is the grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        if (percentage >= 90)
+        {
+            letters = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letters = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letters = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letters = "D";
+        }
+        else
+        {
+            letters = "F";
+        }
+
+        Console.WriteLine(letters);
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations!! You passed!!");
+        }
+        else 
+        {
+            Console.Write("I'm sorry, You will need to retake this class.");
+        }
     }
 }
