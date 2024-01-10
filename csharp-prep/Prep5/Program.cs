da204f0fd392a861c2aa3eef7d28f83b1d304027
anderJoe365 <anderson.joseph3656@gmail.com>
2024-01-10 07:38:10 -0700

    Prep 4 and Prep 5 completed.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squared = SquareNumber(userNumber);
+
+        DisplayResults(userName, squared);
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            return userNumber;
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int squared = userNumber * userNumber;
+
+            return squared;   
+        }
+
+        static void DisplayResults(string userName, int squared)
+        {
+            Console.WriteLine($"{userName}, your number squared is {squared}");
+        }
     }
 }
