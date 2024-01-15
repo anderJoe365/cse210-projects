cbe891f5192d14c75cf80c3d891a7c28657dd9b1
anderJoe365 <anderson.joseph3656@gmail.com>
2024-01-15 10:42:17 -0700

    Resume and Jobs program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "General Manager";
+        job1._company = "Kneaders";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Member Service Representitive";
+        job2._company = "Deseret First Credit Union";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jospeh Anderson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
