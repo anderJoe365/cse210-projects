601da1f589a8f31a49f03df1469b3f53d54a6a6d
anderJoe365 <anderson.joseph3656@gmail.com>
2024-01-30 15:10:41 -0700

    Homework Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment p1 = new Assignment("Sammuel Bennet", "Multiplication");
+        Console.WriteLine(p1.GetSummary());
+
+        MathAssignment p2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(p2.GetSummary());
+        Console.WriteLine(p2.GetHomeworkList());
+
+        WritingAssignment p3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(p3.GetSummary());
+        Console.WriteLine(p3.GetWritingInformation());
+        
     }
 }
